f3a794c7b3352560f7cd1bfeccb4d07a6bacaf02
niplac <nizeyum@gmail.com>
2026-05-31 11:48:30 +0200

    Create LandXmlShift.cs

---
+++ LandXmlShift.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+
+public static class LandXmlShift
+{
+    private const double ShiftX = -32000000.0;
+    private const double ShiftY = 0.0;
+
+    public static void Convert(string inputPath, string outputPath)
+    {
+        XDocument xml = XDocument.Load(inputPath);
+
+        ShiftPntList(xml, "PntList2D", 2);
+        ShiftPntList(xml, "PntList3D", 3);
+        ShiftCoordGeom(xml);
+        UpdateCoordinateSystemNode(xml);
+
+        xml.Save(outputPath);
+    }
+
+    private static void ShiftPntList(XDocument xml, string tag, int dim)
+    {
+        var nodes = xml.Descendants().Where(n => n.Name.LocalName == tag);
+
+        foreach (var n in nodes)
+        {
+            var parts = n.Value.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < parts.Length; i += dim)
+            {
+                double x = double.Parse(parts[i], CultureInfo.InvariantCulture);
+                double y = double.Parse(parts[i + 1], CultureInfo.InvariantCulture);
+
+                x += ShiftX;
+                y += ShiftY;
+
+                parts[i] = x.ToString("F3", CultureInfo.InvariantCulture);
+                parts[i + 1] = y.ToString("F3", CultureInfo.InvariantCulture);
+            }
+
+            n.Value = string.Join(" ", parts);
+        }
+    }
+
+    private static void ShiftCoordGeom(XDocument xml)
+    {
+        var nodes = xml.Descendants().Where(n =>
+            n.Name.LocalName == "Start" ||
+            n.Name.LocalName == "End" ||
+            n.Name.LocalName == "Center" ||
+            n.Name.LocalName == "PI" ||
+            n.Name.LocalName == "Pnt"
+        );
+
+        foreach (var n in nodes)
+        {
+            var parts = n.Value.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length < 2)
+                continue;
+
+            double x = double.Parse(parts[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(parts[1], CultureInfo.InvariantCulture);
+
+            x += ShiftX;
+            y += ShiftY;
+
+            parts[0] = x.ToString("F3", CultureInfo.InvariantCulture);
+            parts[1] = y.ToString("F3", CultureInfo.InvariantCulture);
+
+            n.Value = string.Join(" ", parts);
+        }
+    }
+
+    private static void UpdateCoordinateSystemNode(XDocument xml)
+    {
+        var cs = xml.Descendants().FirstOrDefault(n => n.Name.LocalName == "CoordinateSystem");
+        if (cs == null) return;
+
+        cs.SetAttributeValue("desc", "ETRS89 / UTM zone 32N (EPSG:25832)");
+        cs.SetAttributeValue("horizontalCoordinateSystemName", "EPSG:25832");
+        cs.SetAttributeValue("horizontalDatum", "ETRS89");
+
+        string wkt =
+            "PROJCS[\"ETRS89 / UTM zone 32N\"," +
+            "GEOGCS[\"ETRS89\"," +
+            "DATUM[\"European_Terrestrial_Reference_System_1989\"," +
+            "SPHEROID[\"GRS 1980\",6378137,298.257222101]]," +
+            "PRIMEM[\"Greenwich\",0]," +
+            "UNIT[\"Degree\",0.0174532925199433]]," +
+            "PROJECTION[\"Transverse_Mercator\"]," +
+            "PARAMETER[\"latitude_of_origin\",0]," +
+            "PARAMETER[\"central_meridian\",9]," +
+            "PARAMETER[\"scale_factor\",0.9996]," +
+            "PARAMETER[\"false_easting\",500000]," +
+            "PARAMETER[\"false_northing\",0]," +
+            "UNIT[\"Meter\",1]]";
+
+        cs.SetAttributeValue("ogcWktCode", wkt);
+    }
+}
