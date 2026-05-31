a1d5b6befdc97c0480d00fb306ceb62a7084f297
niplac <nizeyum@gmail.com>
2026-05-31 14:03:12 +0200

    Update LandXmlShift.cs

--- LandXmlShift.cs
+++ LandXmlShift.cs
@@ -11,23 +11,22 @@ public static class LandXmlShift
     {
         XDocument xml = XDocument.Load(inputPath);
 
-        ProcessNodeLists(xml, "PntList2D", 2);
-        ProcessNodeLists(xml, "PntList3D", 3);
-        ProcessSimpleNodes(xml);
+        ShiftList(xml, "PntList2D", 2);
+        ShiftList(xml, "PntList3D", 3);
+        ShiftSimpleNodes(xml);
         UpdateCoordinateSystemNode(xml);
 
         xml.Save(outputPath);
     }
 
-    private static void ProcessNodeLists(XDocument xml, string elementName, int dim)
+    private static void ShiftList(XDocument xml, string tag, int dim)
     {
-        var nodes = xml.Descendants().Where(n => n.Name.LocalName == elementName);
+        var nodes = xml.Descendants().Where(n => n.Name.LocalName == tag);
 
         foreach (var n in nodes)
         {
             var parts = n.Value
-                .Split(new[] { ' ', '\n', '\r', '\t' },
-                StringSplitOptions.RemoveEmptyEntries);
+                .Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
             for (int i = 0; i < parts.Length; i += dim)
             {
@@ -36,31 +35,31 @@ public static class LandXmlShift
 
                 double north, east;
 
-                // Detect which is Easting by its magnitude:
+                // If first value is EASTING (32 million), swap:
                 if (a > b)
                 {
-                    east = a; north = b;
+                    east = a;
+                    north = b;
                 }
                 else
                 {
-                    north = a; east = b;
+                    north = a;
+                    east = b;
                 }
 
-                // Apply shift ONLY to Easting:
+                // Apply only to EASTING
                 east += Shift;
 
-                // Write them back maintaining N E order:
-                parts[i]     = north.ToString("F3", CultureInfo.InvariantCulture);
+                // Write N E back
+                parts[i] = north.ToString("F3", CultureInfo.InvariantCulture);
                 parts[i + 1] = east.ToString("F3", CultureInfo.InvariantCulture);
-
-                // keep Z unchanged if dim == 3
             }
 
             n.Value = string.Join(" ", parts);
         }
     }
 
-    private static void ProcessSimpleNodes(XDocument xml)
+    private static void ShiftSimpleNodes(XDocument xml)
     {
         var nodes = xml.Descendants().Where(n =>
             n.Name.LocalName == "Start" ||
@@ -72,18 +71,28 @@ public static class LandXmlShift
 
         foreach (var n in nodes)
         {
-            var parts = n.Value.Split(new[] { ' ', '\n', '\r', '\t' },
-                                      StringSplitOptions.RemoveEmptyEntries);
+            var parts = n.Value
+                .Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (parts.Length < 2) continue;
+            if (parts.Length < 2)
+                continue;
 
             double a = double.Parse(parts[0], CultureInfo.InvariantCulture);
             double b = double.Parse(parts[1], CultureInfo.InvariantCulture);
 
             double north, east;
 
-            if (a > b) { east = a; north = b; }
-            else       { north = a; east = b; }
+            // swap if first value is larger than second
+            if (a > b)
+            {
+                east = a;
+                north = b;
+            }
+            else
+            {
+                north = a;
+                east = b;
+            }
 
             east += Shift;
 
@@ -102,22 +111,5 @@ public static class LandXmlShift
         cs.SetAttributeValue("desc", "ETRS89 / UTM zone 32N (EPSG:25832)");
         cs.SetAttributeValue("horizontalCoordinateSystemName", "EPSG:25832");
         cs.SetAttributeValue("horizontalDatum", "ETRS89");
-
-        string wkt =
-            "PROJCS[\"ETRS89 / UTM zone 32N\"," +
-            "GEOGCS[\"ETRS89\"," +
-            "DATUM[\"European_Terrestrial_Reference_System_1989\"," +
-            "SPHEROID[\"GRS 1980\",6378137,298.257222101]]," +
-            "PRIMEM[\"Greenwich\",0]," +
-            "UNIT[\"Degree\",0.0174532925199433]]," +
-            "PROJECTION[\"Transverse_Mercator\"]," +
-            "PARAMETER[\"latitude_of_origin\",0]," +
-            "PARAMETER[\"central_meridian\",9]," +
-            "PARAMETER[\"scale_factor\",0.9996]," +
-            "PARAMETER[\"false_easting\",500000]," +
-            "PARAMETER[\"false_northing\",0]," +
-            "UNIT[\"Meter\",1]]";
-
-        cs.SetAttributeValue("ogcWktCode", wkt);
     }
 }
